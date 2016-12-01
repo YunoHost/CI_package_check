7e10c09b0c9ec362a83b148d0ea5ffecca4ec89c
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2016-12-01 10:40:30 +0100

    Couleur et progression

--- analyseCI.sh
+++ analyseCI.sh
@@ -8,8 +8,9 @@ if [ "${0:0:1}" == "/" ]; then script_dir="$(dirname "$0")"; else script_dir="$(
 echo $1 >> "$script_dir/work_list"	# Ajoute le dépôt à tester à la suite de la liste
 APP=$(basename $1)
 
+echo ""
 date
-echo "Attente du début du travail"
+echo "Attente du début du travail..."
 if ! test -e "$script_dir/logs/$APP.log"
 then	# Si c'est la première exécution de ce test, le fichier de log n'existe pas.
 	echo "!!! Attention première exécution du test, l'indication de début de travail est faussée..."
@@ -21,10 +22,16 @@ done
 # Cette double boucle permet de ne pas effacer trop tôt le log précédent. Mais seulement pendant le travail de Package_check sur le package concerné.
 echo ""
 date
-echo -e "Package_check est actuellement en train de tester le package"
+echo "Package_check est actuellement en train de tester le package..."
+nb_print=0
 while ! test -e "$script_dir/logs/$APP.log"; do
 	sleep 30	# Attend que le log soit recréé par le script pcheckCI.sh, ce qui indiquera la fin du test sur ce package.
-	echo -n "."
+	if [ "$nb_print" -gt 1 ]; then
+		cat "$script_dir/package_check/Complete.log" | grep ">>.*Test" | sed "1,"$nb_print"d"	# Affiche les titres de test, en supprimant autant de ligne trouvée que de test déjà affichés. Pour afficher seulement les nouveaux tests.
+	else
+		cat "$script_dir/package_check/Complete.log" | grep ">>.*Test"	# Si il n'y a qu'un seul titre de test trouvé, affiche simplement.
+	fi
+	nb_print=$(cat "$script_dir/package_check/Complete.log" | grep -c ">>.*Test")	# Compte le nombre de titre de test déjà affichés.
 done
 echo ""
 date
--- pcheckCI.sh
+++ pcheckCI.sh
@@ -13,7 +13,7 @@ then	# Le travail est reporté à la prochaine exécution si le lock de package 
 		APP=$(head -n1 "$script_dir/work_list")
 		rm -f "$script_dir/logs/$(basename $APP).log"	# Supprime le log du précédent test.
 		inittime=$(date +%s)	# Enregistre l'heure de démarrage du test
-		"$script_dir/package_check/package_check.sh" --bash-mode $APP &	# Exécute package_check sur la première adresse de la liste, et passe l'exécution en arrière plan.
+		"$script_dir/package_check/package_check.sh" --bash-mode $APP > "$script_dir/package_check/Test_results_cli.log" &	# Exécute package_check sur la première adresse de la liste, et passe l'exécution en arrière plan.
 		PID_PCHECK=$!	# Récupère le PID de la commande package_check
 		while ps -p $PID_PCHECK | grep -q $PID_PCHECK	# Boucle tant que le process tourne, pour vérifier le temps d'exécution
 		do
@@ -26,7 +26,7 @@ then	# Le travail est reporté à la prochaine exécution si le lock de package 
 			sleep 30
 		done
 		sed -i 1d "$script_dir/work_list"	# Supprime la première ligne de la liste
-		cp "$script_dir/package_check/Test_results.log" "$script_dir/logs/$(basename $APP).log"
+		cp "$script_dir/package_check/Test_results_cli.log" "$script_dir/logs/$(basename $APP).log"
 		if [ "$inittime" -eq "0" ]; then
 			echo "!!! L'exécution de Package_check a été trop longue, le script a été avorté. !!!" >> "$script_dir/logs/$(basename $APP).log"
 		fi
