bcb1eee8df636cb0f2eda6680a25b9a1a0d20bf9
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2017-03-05 00:57:36 +0100

    Fix config package check

--- lxc_stop.sh
+++ lxc_stop.sh
@@ -5,8 +5,9 @@
 # Récupère le dossier du script
 if [ "${0:0:1}" == "/" ]; then script_dir="$(dirname "$0")"; else script_dir="$(echo $PWD/$(dirname "$0" | cut -d '.' -f2) | sed 's@/$@@')"; fi
 
-PLAGE_IP=$(cat "$script_dir/package_check/sub_scripts/lxc_build.sh" | grep PLAGE_IP= | cut -d '=' -f2)
-LXC_NAME=$(cat "$script_dir/package_check/sub_scripts/lxc_build.sh" | grep LXC_NAME= | cut -d '=' -f2)
+pcheck_config="$script_dir/package_check/config"
+PLAGE_IP=$(cat "$pcheck_config" | grep PLAGE_IP= | cut -d '=' -f2)
+LXC_NAME=$(cat "$pcheck_config" | grep LXC_NAME= | cut -d '=' -f2)
 
 echo "> Arrêt du conteneur"
 if [ $(sudo lxc-info --name $LXC_NAME | grep -c "STOPPED") -eq 0 ]; then
