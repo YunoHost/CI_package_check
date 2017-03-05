39ad976a58f56920c5351ef5cfc6967775cb7e22
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2017-03-05 18:33:39 +0100

    Work on scaleway

---
+++ fast_build/Copie CI.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Récupère le dossier du script
+if [ "${0:0:1}" == "/" ]; then script_dir="$(dirname "$0")"; else script_dir="$(echo $PWD/$(dirname "$0" | cut -d '.' -f2) | sed 's@/$@@')"; fi
+
+ssh_user=SSH_USER
+ssh_host=SSH_HOST
+ssh_port=22
+distant_dir=/home
+
+ENVOI_CI () {
+	echo ">>> Copie de $1"
+	rsync -avzhuE --progress --delete --exclude="^.git.*" "$1" -e 'ssh -p $ssh_port' $ssh_user@$ssh_host:"$distant_dir/"
+}
+
+## Options rsync:
+# -a archive (rlptgoD)
+# -v verbose
+# --delete, supprime les fichiers absent dans la source
+# -b backup
+# -z compression des données pour le transfert
+# -h human readable
+# --progress
+# -u update, ne remplace pas un fichier si il est plus récent que la source
+# -e ssh login@serveur_ip_ou_nom: 	pour utiliser ssh
+# -E preserve executability
+
+scaleway_api.sh start	# Démarre le serveur scaleway distant avant de se connecter
+
+ENVOI_CI DOSSIER_DE_MON_APP_1
+ENVOI_CI DOSSIER_DE_MON_APP_2
---
+++ fast_build/scaleway_api.sh
@@ -0,0 +1,84 @@
+#/bin/bash
+
+token=TOKEN_SCALEWAY
+
+#Serveur de base: VC1S Jessie
+server_id=SERVER_ID
+
+SERVER_STATUS () {
+		# Surveille l'état du serveur
+		curl -sS \
+		-H "X-Auth-Token: $token" \
+		-H "Content-Type: application/json" \
+		https://api.cloud.online.net/servers/$server_id \
+		| json_pp \
+		| grep "\"state\"" | cut -d '"' -f 4
+		# json_pp permet de mettre en forme le json en sortie de curl
+}
+
+SERVER_CHECK_STATUS () {
+	server_state=-1
+	sleep 2
+	while [ $server_state -ne 0 ] && [ $server_state -ne 1 ]
+	do
+		# Surveille l'état du serveur
+		server_state=$(SERVER_STATUS)
+		# json_pp permet de mettre en forme le json en sortie de curl
+		echo "> $server_state"
+		if [ "$server_state" = "running" ]; then
+			server_state=1
+			echo -e "\e[1m>> Le serveur a démarré\e[0m"
+		elif [ "$server_state" = "stopped" ]; then
+			server_state=0
+			echo -e "\e[1m>> Le serveur est arrêté\e[0m"
+		else
+			server_state=-1
+			sleep 30
+		fi
+	done
+}
+
+SERVER_START () {
+	server_state=$(SERVER_STATUS)
+	if [ "$server_state" = "running" ]; then
+		echo -e "\e[1m>> Le serveur est déjà démarré\e[0m"
+	else
+		echo -e "\e[1m> Démarrage du serveur\e[0m"
+		curl -sS \
+		-H "X-Auth-Token: $token" \
+		-H "Content-Type: application/json" \
+		https://api.cloud.online.net/servers/$server_id/action \
+		-d '{"action": "poweron"}' \
+		| json_pp
+
+		SERVER_CHECK_STATUS
+	fi
+}
+
+SERVER_STOP () {
+	server_state=$(SERVER_STATUS)
+	if [ "$server_state" = "running" ]; then
+		echo -e "\e[1m>> Le serveur est déjà arrêté\e[0m"
+	else
+		echo -e "\e[1m> Arrêt du serveur\e[0m"
+		curl -sS \
+		-H "X-Auth-Token: $token" \
+		-H "Content-Type: application/json" \
+		https://api.cloud.online.net/servers/$server_id/action \
+		-d '{"action": "poweroff"}' \
+		| json_pp
+
+		SERVER_CHECK_STATUS
+	fi
+}
+
+if [ "$1" = "start" ]
+then
+	SERVER_START
+elif [ "$1" = "stop" ]
+then
+	SERVER_STOP
+elif [ "$1" = "status" ]
+then
+	SERVER_STATUS
+fi
