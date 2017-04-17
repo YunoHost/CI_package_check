44d093e3291cdb57ddd7e31b42d77e9a6c448fdf
Maniack Crudelis <maniack_crudelis@yahoo.fr>
2017-04-17 18:24:13 +0200

    [ref] compare_level, list_app_ynh, switch_container and rapportCI.sh
    Globally just a refactoring.
    
    And a small correction on the ynh_version file for non stable test

--- auto_build/compare_level.sh
+++ auto_build/compare_level.sh
@@ -1,96 +1,135 @@
 #!/bin/bash
 
-# Récupère le dossier du script
+#=================================================
+# Grab the script directory
+#=================================================
+
 if [ "${0:0:1}" == "/" ]; then script_dir="$(dirname "$0")"; else script_dir="$(echo $PWD/$(dirname "$0" | cut -d '.' -f2) | sed 's@/$@@')"; fi
 
-app=$1	# Le script prend en 1er argument le nom du test
-app_log=$2	# Et en 2e, le log de l'app
+#=================================================
+# Get variables
+#=================================================
 
-echo "app=$app"
-echo "app_log=$app_log"
+# The first argument is the name of the test. The third field of work_list
+test_name=$1
+# The second argument is the relative path of the log for this app
+app_log=$2
 
-ADD_LEVEL_IN_LIST () {
-	list_name=$1
-	list_file="$script_dir/$list_name"
-	if [ -n "$app_level" ]
-	then	# Si le log contient un niveau pour l'app
-		if [ ! -e "$list_file" ]; then
-			touch "$list_file"	# Créer la liste si le fichier n'existe pas.
-		fi
-		if grep -q "$app" "$list_file"; then
-			sed -i "s/$app:.*/$app:$app_level/" "$list_file"	# Remplace le niveau de l'app dans la liste
-		else	# Ou si l'app n'est pas déjà dans le fichier
-			echo "$app:$app_level" >> "$list_file"	# Inscrit le level de l'app dans la liste
-		fi
-	fi
-}
-
-if [ ! -e "$script_dir/list_level_stable" ]
-then	# Si le fichier list_level_stable n'existe pas, c'est la première exécution
-	while read line	# Le fichier list_level_stable est constitué une première fois à partir des logs existants
-	do
-		if ! grep -q "_complete.log$" <<< "$line"	# Ignore les logs "complete", ne traite que les autres fichiers.
-		then
-			app=$(grep "^-> Test " < "$script_dir/../logs/$line")	# Récupère le nom du test, ajouté au début du log
-			app=${app#-> Test }	# Supprime "-> Test " pour garder uniquement le nom du test
-			app_level=$(tac "$script_dir/../logs/$line" | grep "Niveau de l'application: " -m1)	# Tac affiche le fichier depuis la fin, et grep limite la recherche au premier terme trouvé pour ne prendre que le dernier résultat.
-			if [ -n "$app_level" ]
-			then	# Si le log contient un niveau pour l'app
-				app_level="$(echo $(expr match "$app_level" '.*\(.[0-9]*\)'))"	# Extrait uniquement la valeur numérique du résultat avec match.
-				echo "$app:$app_level" >> "$script_dir/list_level_stable"	# Inscrit le level de l'app dans le fichier list_level_stable
-			fi
-		fi
-	done <<< "$(ls -1 "$script_dir/../logs")"
+# Identify the type of test
+if echo "$test_name" | grep --quiet "(testing)"; then
+	type=testing
+elif echo "$test_name" | grep --quiet "(unstable)"; then
+	type=unstable
 else
-	# Récupère le niveau de l'application
-	app_level=$(tac "$script_dir/../logs/$app_log" | grep "Niveau de l'application: " -m1)	# Tac affiche le fichier depuis la fin, et grep limite la recherche au premier terme trouvé pour ne prendre que le dernier résultat.
-	if [ -n "$app_level" ]
-	then	# Si le log contient un niveau pour l'app
-		app_level="$(echo $(expr match "$app_level" '.*\(.[0-9]*\)'))"	# Extrait uniquement la valeur numérique du résultat avec match.
+	type=stable
+fi
+
+#=================================================
+# Get the level from the log
+#=================================================
+
+# Find the line which contain the level
+app_level="$(tac "$script_dir/../logs/$app_log" | grep "^Level of this application: " --max-count=1)"
+# And keep only the level
+app_level="${app_level##*: }"
+
+#=================================================
+# Store the level in a list
+#=================================================
+
+# Each type have its own list
+list_file="$script_dir/list_level_$type"
+
+# If a level has been found
+if [ -n "$app_level" ]
+then
+
+	# Create the list if it doesn't exist
+	if [ ! -e "$list_file" ]; then
+		touch "$list_file"
 	fi
-	# Traite l'information selon le type de test
-	if echo "$app" | grep -q "(testing)\|(unstable)"	# Vérifie si c'est un test testing ou unstable
-	then	# Si c'est un test sur testing ou unstable, compare avec stable
-		if echo "$app" | grep -q "(testing)"; then
-			type=testing
-		else
-			type=unstable
-		fi
-		echo "Test sur $type"
-		ADD_LEVEL_IN_LIST list_level_$type
-		if [ -s "$script_dir/../work_list" ]; then	# Si la file d'attente n'est pas vide
-			if grep -q "$type" "$script_dir/../work_list"
-			then
-				echo "Un autre test sur $type va démarrer"
-				exit 0  # Le script a terminé son travail
-			fi
-		fi
-		# Si la file d'attente est vide, la série de test est terminée.
-		> "$script_dir/mail_diff_level"
-		while read line	# Compare chaque niveau avec le niveau en stable
-		do
-			app=$(echo ${line%:*})	# Supprime après les : pour garder le nom de l'app
-			app=$(echo ${app% \($type\)})	# Et supprime le type, testing ou unstable
-			app_level=$(echo ${line##*:})	# Et avant les : pour garder le niveau seulement
-			stable_level=$(grep "$app" "$script_dir/list_level_stable" | cut -d: -f2)	# Récupère le niveau de l'app dans la liste stable
-			if [ "$app_level" -ne "$stable_level" ]	# Si le niveau est différent
-			then
-				echo "- Changement de niveau de l'app $app de $stable_level en stable vers $app_level en $type." >> "$script_dir/mail_diff_level"
-			fi
-		done < "$script_dir/list_level_$type"
-	else	# Si c'est un test sur stable, modifie les niveaux de référence
-		echo "Test sur stable"
-		ADD_LEVEL_IN_LIST list_level_stable
+
+	# Try to find this app in the list
+	if grep --quiet "^$test_name:" "$list_file"
+	then
+
+		# If the app has been found, replace the level
+		sed --in-place "s/^$test_name:.*/$test_name:$app_level/" "$list_file"
+
+	# Else, add this app to the list
+	else
+		echo "$test_name:$app_level" >> "$list_file"
 	fi
 fi
 
-if [ -s "$script_dir/mail_diff_level" ]; then	# Si le mail n'est pas vide
-# 	mail -s "Différences de niveaux entre stable et $type" "$dest" < "$script_dir/mail_diff_level"	# Envoi le différentiel de niveau par mail
-	if [ $(wc -l "$script_dir/mail_diff_level" | cut -d' ' -f1) -gt 1 ]
-	then	# En cas de message sur plusieurs lignes, je sais pas comment faire...
-		paste=$(cat "$script_dir/mail_diff_level" | yunopaste)
-		echo "Différences de niveaux entre stable et $type: $paste" > "$script_dir/mail_diff_level"
+#=================================================
+# For testing and unstable, compare with stable
+#=================================================
+
+message_file="$script_dir/diff_level_send"
+
+if [ "$type" = "testing" ] || [ "$type" = "unstable" ]
+then
+
+	# Check if another test on the same container is waiting in the work_list
+	if grep --quiet "($type)" "$script_dir/../work_list"
+	then
+		# Finish the script, the next execution will continue.
+		exit 0
 	fi
-	"$script_dir/xmpp_bot/xmpp_post.sh" "$(cat "$script_dir/mail_diff_level")"	# Notifie sur le salon apps
-	rm "$script_dir/list_level_$type"
+
+	# Compare each app for this type with the level in stable
+	# Read each app added in the list
+	while read line
+	do
+		# Get the name of the test, from the list
+		test_name=$(echo ${line%:*})
+		# And remove the type, to keep only the name of the app
+		test_name=$(echo ${test_name% \($type\)})
+		# Get the level
+		app_level=$(echo ${line##*:})
+
+		# Get the level in the stable list
+		stable_level=$(grep "^$test_name:" "$script_dir/list_level_stable" | cut --delimiter=: --fields=2)
+
+		# Compare the levels
+		if [ "$app_level" -ne "$stable_level" ]
+		then
+			# If the levels are different, add a line to the message to send
+			echo "- Application $test_name change from $stable_level in stable to $app_level in $type. (domain/path/log...)" >> "$message_file"
+		fi
+	done < "$list_file"
+
+	# Remove the list after the comparaison
+	rm "$list_file"
 fi
+
+#=================================================
+# Notify on xmpp apps room
+#=================================================
+
+# If the message file is not empty
+if [ -s "$message_file" ]
+then
+
+	# If the message has more than one line, store it into a pastebin
+	if [ $(wc --lines "$message_file" | cut --delimiter=' ' --fields=1) -gt 1 ]
+	then
+		# Store the message into a pastebin
+		paste=$(cat "$message_file" | yunopaste)
+		# And send only its adress
+		echo "Level difference between stable and $type: $paste" > "$message_file"
+	fi
+
+	xmpppost="$script_dir/xmpp_bot/xmpp_post.sh"
+	# Send via xmpp only if the script was find
+	if [ -e "$xmpppost" ]
+	then
+		"$xmpppost" "$(cat "$message_file")"
+
+		# Remove the list of levels and the message file
+		rm "$script_dir/list_level_$type"
+		rm "$message_file"
+	fi
+fi
+
