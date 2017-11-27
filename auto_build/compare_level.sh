0c9631c8a93fe94db919c495fdf107b4054f0104
Maniack Crudelis <maniackc_dev@crudelis.fr>
2017-11-27 19:45:31 +0100

    Public level list

--- auto_build/compare_level.sh
+++ auto_build/compare_level.sh
@@ -67,6 +67,98 @@ then
 fi
 
 #=================================================
+# Store the level and other infos in a public list
+#=================================================
+
+# Check the global result of the test
+# Success by default
+success=1
+# Search for some FAIL in the final results
+# But, ignore the line of package linter.
+if grep "FAIL$" "$script_dir/../logs/$app_log" | grep --invert-match "Package linter" | grep --quiet "FAIL$"
+then
+	# If a fail was find, the test failed.
+	success=0
+# Search also for a "PCHECK_AVORTED". That means the script pcheckCI was aborted by a timeout.
+elif grep "PCHECK_AVORTED" "$script_dir/../logs/$app_log"
+then
+	success=0
+# And, finally, check if at least one test was a success.
+elif ! grep "SUCCESS$" "$script_dir/../logs/$app_log" | grep --invert-match "Package linter" | grep --quiet "SUCCESS$"
+then
+	success=0
+fi
+
+# Declare an array of 16 cells, for the result of each tests
+tests_results=(? ? ? ? ? ? ? ? ? ? ? ? ? ? ? ?)
+
+# If no level found
+if [ -z "$app_level" ]; then
+	app_level="?"
+else
+	# Find the detailled results of this test
+	while read detailled_level
+	do
+		# Remove the 4 first characters, which are only a color indication
+		detailled_level="${detailled_level:4}"
+		case "${detailled_level%%:*}" in
+			"age linter" ) index=0 ;;
+			"Installation" ) index=1 ;;
+			"Deleting" ) index=2 ;;
+			"Installation in a sub path" ) index=3 ;;
+			"Deleting from a sub path" ) index=4 ;;
+			"Installation on the root" ) index=5 ;;
+			"Deleting from root" ) index=6 ;;
+			"Upgrade" ) index=7 ;;
+			"Installation in private mode" ) index=8 ;;
+			"Installation in public mode" ) index=9 ;;
+			"Multi-instance installations" ) index=10 ;;
+			"Malformed path" ) index=11 ;;
+			"Port already used" ) index=12 ;;
+			"Backup" ) index=13 ;;
+			"Restore" ) index=14 ;;
+			"Change URL" ) index=15 ;;
+			*) index=-1 ;;
+		esac
+
+		if [ $index -ge 0 ]
+		then
+			if echo "$detailled_level" | grep --quiet "SUCCESS"; then
+				# Can be a success only if there no failure on this test before.
+				if [ "${tests_results[$index]}" != "0" ]; then
+					tests_results[$index]=1
+				fi
+			elif echo "$detailled_level" | grep --quiet "FAIL"; then
+				tests_results[$index]=0
+			else
+				if [ "${tests_results[$index]}" == "?" ]; then
+					# Result unknow if there no success or fail before.
+					tests_results[$index]="-"
+				fi
+			fi
+		fi
+	done <<< "$(tac "$script_dir/../logs/$app_log" | grep "Level of this application: " --after-context=20)"
+fi
+
+# Each type have its own list
+public_list_file="$script_dir/../logs/list_level_$type"
+
+# Remove the previous entry for this app
+sed --in-place "/^$test_name;/d" "$public_list_file"
+sed --in-place "/^\"$test_name\":/d" "$public_list_file.json"
+
+# Then add this app to the list
+echo "$test_name;level=$app_level;success=$success;detailled_success=${tests_results[@]};date=$(date) ($(date +%s))" >> "$public_list_file"
+
+# Rebuild the array for the (fucking ?) json format
+for i in ${tests_results[*]}
+do
+	tests_results_json="$tests_results_json \"$i\","
+done
+# Remove the last comma
+tests_results_json=${tests_results_json%,}
+echo "{ \"$test_name\": { \"level\": $app_level, \"success\": $success, \"detailled_success\": [$tests_results_json ], \"date\": \"$(date)\", \"timestamp\": $(date +%s) } }" >> "$public_list_file.json"
+#=================================================
 # For testing and unstable, compare with stable
 #=================================================
 
