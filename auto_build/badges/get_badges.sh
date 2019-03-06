4d70ddd68e1c5d9962ce00c1a69b74458e1f9206
Kayou <pierre.moltess@gmail.com>
2019-03-06 17:34:13 +0100

    Add badge level 8

--- auto_build/badges/get_badges.sh
+++ auto_build/badges/get_badges.sh
@@ -11,4 +11,5 @@ wget -nv https://img.shields.io/badge/Integration-Level_4-yellowgreen.svg -O "$s
 wget -nv https://img.shields.io/badge/Integration-Level_5-green.svg -O "$script_dir/level5.svg"
 wget -nv https://img.shields.io/badge/Integration-Level_6-green.svg -O "$script_dir/level6.svg"
 wget -nv https://img.shields.io/badge/Integration-Level_7-brightgreen.svg -O "$script_dir/level7.svg"
+wget -nv https://img.shields.io/badge/Integration-Level_8-brightgreen.svg -O "$script_dir/level8.svg"
 wget -nv https://img.shields.io/badge/Integration-Unknown-lightgrey.svg -O "$script_dir/unknown.svg"
