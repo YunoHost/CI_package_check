410d6362f3ebbdd91cc473dc0d33878e23181fd1
Maniack Crudelis <maniackc_dev@crudelis.fr>
2019-03-09 18:11:49 +0100

    Move kill package_check to lxc_force_stop

--- force_stop.sh
+++ force_stop.sh
@@ -16,17 +16,7 @@ echo Finish > "$lock_pcheckCI"
 # Remove this test of the work_list
 sudo sed --in-place "/$id/d" "$script_dir/work_list"
 
-# Kill package_check
-# Retrieve the pid of Package check
-get_pid="ps -C package_check.sh -o pid="
-
-# Loop while package_check is still running
-while $get_pid > /dev/null
-do
-	sudo kill --signal 15 $($get_pid | head --line=1)
-done
-
-# Stop the container and its network
+# Stop package_check, the container and its network
 sudo "$script_dir/package_check/sub_scripts/lxc_force_stop.sh"
 
 # Wait for the cleaning of the lock file. That means analyseCI.sh finished on its side.
@@ -42,5 +32,4 @@ do
 done
 
 # Remove the lock files
-sudo rm "$script_dir/package_check/pcheck.lock"
 sudo rm "$lock_pcheckCI"
