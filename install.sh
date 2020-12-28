d0556a96677e62bdffd0342b7869649bad35618a
Alexandre Aubin <alex.aubin@mailoo.org>
2020-12-28 03:05:13 +0100

    Trash jenkins stuff, use yunorunner for dev CI

--- install.sh
+++ install.sh
@@ -14,20 +14,16 @@ then
     exit 1
 fi
 
-default_ci_user=ynhci
 
-# This script can get as argument the domain for jenkins, the admin password of YunoHost and the type of installation requested.
-# Domain for Jenkins
+# This script can get as argument the domain for yunorunner, the admin password of YunoHost and the type of installation requested.
 domain=$1
-# Admin password of YunoHost
 yuno_pwd=$2
-# Type of CI to setup.
 ci_type=$3
 if [ -z "$ci_type" ]
 then
 	echo "Please choose the type of CI to build"
-	echo -e "\t1) Yunorunner"
-    echo -e "\t2) Dev (jenkins + chroots)"
+    echo -e "\t1) Regular (jobs from apps.json, etc..)"
+    echo -e "\t2) Dev (jobs from ssh chroots folders)"
 	read -p "?: " answer
 fi
 case $answer in
@@ -77,8 +73,10 @@ function setup_yunohost() {
     echo_bold "> Running yunohost postinstall"
 	yunohost tools postinstall --domain $domain --password $yuno_pwd
     
-    echo_bold "> Create CI user"
-    yunohost user create --firstname "$default_ci_user" --mail "$default_ci_user@$domain" --lastname "$default_ci_user" "$default_ci_user" --password $yuno_pwd
+    # What is it used for :| ...
+    echo_bold "> Create Yunohost CI user"
+    local ynh_ci_user=ynhci
+    yunohost user create --firstname "$ynh_ci_user" --mail "$ynh_ci_user@$domain" --lastname "$ynh_ci_user" "$ynh_ci_user" --password $yuno_pwd
 
     # Idk why this is needed but wokay I guess >_>
     echo -e "\n127.0.0.1 $domain	#CI_APP" >> /etc/hosts
@@ -128,13 +126,18 @@ CI_URL=$domain/$ci_path
 EOF
 
     # Cron tasks
+    cat >>  "/etc/cron.d/CI_package_check" << EOF
+# Autoupgrade every night
+0 3 * * * root "/home/CI_package_check/auto_upgrade.sh" >> "/home/CI_package_check/auto_upgrade.log" 2>&1
+EOF
+    
     if [ $ci_type == "Dev" ]
     then
-        cron_file="./lib/cron_dev"
-    elif
-        cron_file="./lib/cron_yunorunner"
+    cat >>  "/etc/cron.d/CI_package_check" << EOF
+# Vérifie toutes les 5 minutes si une nouvelle app a été ajoutée.
+*/5 * * * * root "/home/CI_package_check/lib/dev_ci_scan_jobs.sh" >> "/home/CI_package_check/lib/dev_ci_scan_jobs.log" 2>&1
+EOF
     fi
-    cp "$cron_file" "/etc/cron.d/CI_package_check"
 
     # Add permission to the user for the entire CI_package_check because it'll be the one running the tests (as a non-root user)
     chown -R $ci_user ./
@@ -148,11 +151,16 @@ install_dependencies
 
 [ -e /usr/bin/yunohost ] || setup_yunohost
 
+source lib/setup_yunorunner.sh
+
 if [ $ci_type == "Dev" ]
-then
-    source setup_jenkins_and_chroot.sh
-else
-    source setup_yunorunner.sh
+    # Installation de ssh_chroot_dir
+    yunohost app install --force https://github.com/YunoHost-Apps/ssh_chroot_dir_ynh -a "ssh_user=base_user&password=""&pub_key=fake_key&size=1G"
+
+    # Créer un lien symbolique pour un accès facile à chroot_manager
+    ln -sf /home/yunohost.app/ssh_chroot_directories/chroot_manager ./chroot_manager
+    # Et à l'ajout d'utilisateur.
+    ln -sf "lib_dev/Add_a_new_user.sh" ./Add_a_new_user.sh
 fi
 
 setup_lxd
