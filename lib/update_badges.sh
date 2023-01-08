12e6599465a96172e8a033d56cdea28b78195ad1
Alexandre Aubin <alex.aubin@mailoo.org>
2023-01-08 18:57:15 +0100

    Update/simplify badge stuff

--- lib/update_badges.sh
+++ lib/update_badges.sh
@@ -34,37 +34,13 @@ then
     # Parse each app into the list
     while read app
     do
-        # Get the high_quality tag for this app
-        high_quality=$(jq --raw-output ".apps[\"$app\"] | .high_quality" "$script_dir/apps.json")
-
         # Get the status for this app
         state=$(jq --raw-output ".apps[\"$app\"] | .state" "$script_dir/apps.json")
         # Get the maintained status for this app
-        maintained=$(jq --raw-output ".apps[\"$app\"] | .maintained" "$script_dir/apps.json")
+        maintained=$(jq --raw-output ".apps[\"$app\"] | .antifeatures | .[]" "$script_dir/apps.json" | grep -q 'package-not-maintained' && echo unmaintained || echo maintained)
 
-        # Update the tag for the status
-        if [ "$high_quality" == "true" ] && [ "$state" == "working" ] && [ "$maintained" == "true" ]
-        then
-            # If the app has the high_quality tag, is working and is maintained.
-            # Then, the app si a high quality one and earn its tag
-            state_badge=high_quality
-        else
-            state_badge=$state
-        fi
-
-        # Update the tag for the maintain status
-        if [ "$maintained" == "true" ]
-        then
-            maintain_badge=maintained
-        elif [ "$maintained" == "false" ]
-        then
-            maintain_badge=orphaned
-        else
-            maintain_badge=$maintained
-        fi
-
-        cp "$script_dir/../badges/$state_badge.svg" "$script_dir/../badges/${app}.status.svg"
-        cp "$script_dir/../badges/$maintain_badge.svg" "$script_dir/../badges/${app}.maintain.svg"
+        cp "$script_dir/../badges/$state.svg" "$script_dir/../badges/${app}.status.svg"
+        cp "$script_dir/../badges/$maintained.svg" "$script_dir/../badges/${app}.maintain.svg"
 
     # List all apps from the list, by getting manifest ID.
     done <<< "$(jq --raw-output ".apps[] | .manifest.id" "$script_dir/apps.json")"
