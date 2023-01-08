298c9ac2e8d687d4335cf76e41b493af337c3206
Alexandre Aubin <alex.aubin@mailoo.org>
2023-01-08 20:58:25 +0100

    Fix instructions for matrix-commander setup

--- lib/chat_notify.sh
+++ lib/chat_notify.sh
@@ -11,11 +11,14 @@
 # pip3 install matrix-commander
 # chmod 700 "$MCHOME"
 # matrix-commander $MCARGS --login password   # < NB here this is literally 'password' as authentication method, the actual password will be asked by a prompt
-# chmod 600 "$MCHOME/credentials.json"
-# chmod 700 "$MCHOME"
 # matrix-commander $MCARGS --room-join '#yunohost-apps:matrix.org'
+#
+# groupadd matrixcommander
+# usermod -a -G  matrixcommander yunorunner
+# chgrp -R matrixcommander $MCHOME
+# chmod -R 770 $MCHOME
 #
 
 MCHOME="/opt/matrix-commander/"
 MCARGS="-c $MCHOME/credentials.json --store $MCHOME/store"
-"$MCHOME/venv/bin/matrix-commander" $MCARGS -m "$@"  --room 'yunohost-apps'
+timeout 10 "$MCHOME/venv/bin/matrix-commander" $MCARGS -m "$@"  --room 'yunohost-apps'
